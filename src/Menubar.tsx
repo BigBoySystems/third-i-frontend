683a6d121bf7314d0bca8190af1e84eaf4b7eeb4
Yohan Boogaert <yozhgoor@outlook.com>
2020-08-01 10:31:44 +0200

    Complete Menubar items (#1)

--- src/Menubar.tsx
+++ src/Menubar.tsx
@@ -7,6 +7,8 @@ import {
   PanelStack,
   IPanel,
   IPanelProps,
+  Radio, 
+  RadioGroup,
 } from "@blueprintjs/core";
 
 function Menubar() {
@@ -53,18 +55,117 @@ function MenubarSettings({ openPanel }: IPanelProps) {
         }
       />
       <MenuItem icon="fullscreen" text="Resolution" />
+      <MenuItem icon="camera" text="Photo/Video Mode" />
+      <MenuItem icon="mobile-video" text="Streaming settings" />
+      <MenuItem icon="headset" text="Audio settings" />
+      <MenuItem
+        icon="square"
+        text="Viewing Angle"
+        onClick={() =>
+          openPanel({ component: MenubarAngle, title: "Viewing Angle"})
+        }
+        />
+      <MenuItem
+        icon="lightbulb"
+        text="Lightning"
+        onClick={() =>
+          openPanel({ component: MenubarLightning, title: "Lighting"})
+        }
+        />
       <MenuDivider />
-      <MenuItem icon="cog" text="Advanced parameters" />
+      <MenuItem 
+        icon="cog" 
+        text="Advanced parameters"
+        onClick={() =>
+        openPanel({ component : MenuAdvanced, title: "Advanced parameters"})
+        }
+      />
     </Menu>
   );
 }
 
 function MenubarDisplay() {
+  const [radioCheck, setRadioCheck] = useState("3dFlat");
+
+  return (
+    <RadioGroup
+      label="Display Choice"
+      onChange={(event) => setRadioCheck(event.currentTarget.value)}
+      selectedValue={radioCheck}>
+      <Radio label="3D flat" value="3dFlat" />
+      <Radio label="3D distored" value="3dDistored" />
+      <Radio label="2D left only" value="2dLeftOnly" />
+      <Radio label="2D right only" value="2dRightOnly" />
+      <Radio label="Anaglyph" value="anaglyph" />
+      <Radio label="Inverted" value="inverted" />
+      <Radio label="Flipped" value="flipped" />
+    </RadioGroup>
+  );
+}
+
+function MenubarAngle() {
+  const [radioCheck, setRadioCheck] = useState("extended");
+
+  return (
+    <RadioGroup
+      label="Display Choice"
+      onChange={(event) => setRadioCheck(event.currentTarget.value)}
+      selectedValue={radioCheck}>
+      <Radio label="Extended" value="extended" />
+      <Radio label="Square" value="square" />
+      <Radio label="Custom" value="custom" />
+    </RadioGroup>
+  );
+}
+
+function MenubarLightning() {
+  const [radioCheck, setRadioCheck] = useState("nightOutside");
+
+  return (
+    <RadioGroup
+      label="Display Choice"
+      onChange={(event) => setRadioCheck(event.currentTarget.value)}
+      selectedValue={radioCheck}>
+      <Radio label="Night outside" value="nightOutside" />
+      <Radio label="Day inside" value="dayInside" />
+      <Radio label="Night inside" value="nightInside" />
+      <Radio label="Day outside" value="dayOutside" />
+    </RadioGroup>
+  );
+}
+
+function MenuAdvanced({ openPanel }: IPanelProps) {
   return (
     <Menu>
-      <MenuItem icon="desktop" text="3D flat" />
-      <MenuItem icon="fullscreen" text="2D left only" />
-      <MenuItem icon="fullscreen" text="2D right only" />
+      <MenuItem icon="refresh" text="Loop record" />
+      <MenuItem icon="dashboard" text="Framerate" />
+      <MenuItem icon="globe-network" text="Wifi settings" />
+      <MenuItem icon="power" text="Auto shut off" />
+      <MenuItem icon="lock" text="Auto Standby" />
+      <MenuItem icon="time" text="Timer" />
+      <MenuItem 
+        icon="media" 
+        text="Picture settings"
+        onClick={() =>
+        openPanel({ component : MenuPicture, title: "Picture settings"})
+        }
+      />
+      <MenuItem icon="pulse" text="Bitrate" />
+      <MenuItem icon="wrench" text="Factory reset" />
+      <MenuItem icon="updated" text="Update" />
+    </Menu>
+  );
+}
+
+function MenuPicture() {
+  return (
+    <Menu>
+      <MenuItem icon="wrench" text="WB" />
+      <MenuItem icon="flash" text="Exposure" />
+      <MenuItem icon="contrast" text="Contrast" />
+      <MenuItem icon="delta" text="Sharpness" />
+      <MenuItem icon="pivot-table" text="Stabilization" />
+      <MenuItem icon="cell-tower" text="Av gain" />
     </Menu>
   );
 }
