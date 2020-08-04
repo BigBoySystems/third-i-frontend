80fe587ef1c1a56c1eb54f8b3b758b59ca3ec25c
Yohan Boogaert <yozhgoor@outlook.com>
2020-08-04 06:54:52 +0200

    More features in Settings (#2)

--- src/Menubar.tsx
+++ src/Menubar.tsx
@@ -63,7 +63,11 @@ function MenubarSettings({ openPanel }: IPanelProps) {
         labelElement={<Icon icon={photoMode ? "media" : "mobile-video"} />}
         onClick={() => setPhotoMode(!photoMode)}
       />
-      <MenuItem icon="mobile-video" text="Streaming settings" />
+      <MenuItem 
+        icon="mobile-video" 
+        text="Streaming settings" 
+        onClick={() => openPanel({ component: MenubarStreaming, title: "Streaming settings" })}
+        />
       <MenuItem icon="headset" text="Audio settings" />
       <MenuItem
         icon="square"
@@ -104,6 +108,29 @@ function MenubarDisplay() {
       </RadioGroup>
       <Switch label="Inverted" checked={inverted} onChange={() => setInverted(!inverted)} />
       <Switch label="Flipped" checked={flipped} onChange={() => setFlipped(!flipped)} />
+    </div>
+  );
+}
+
+function MenubarStreaming () {
+  const [browser, setBrowser] = useState(false);
+  const [udp, setUdp] = useState(false);
+  const [usb, setUsb] = useState(false);
+  const [rtmp, setRtmp] = useState(false);
+  const [mpeg, setMpeg] = useState(false);
+  const [rtsp, setRtsp] = useState(false);
+
+  return (
+    <div className="Menubar-content">
+      <Switch label="Browser stream" checked={browser} onChange={() => setBrowser(!browser)} />
+      <Switch label="Stream UDP" checked={udp} onChange={() => setUdp(!udp)} />
+      <MenuItem icon="mobile-video" text="UDP clients" />
+      <Switch label="USB enabled" checked={usb} onChange={() => setUsb(!usb)} />
+      <Switch label="RTMP enabled" checked={rtmp} onChange={() => setRtmp(!rtmp)} />
+      <MenuItem icon="mobile-video" text="RTMP URL" />
+      <Switch label="MPEG-TS enabled" checked={mpeg} onChange={() => setMpeg(!mpeg)} />
+      <MenuItem icon="mobile-video" text="MPEG-TS clients" />
+      <Switch label="RTSP enabled" checked={rtsp} onChange={() => setRtsp(!rtsp)} />
     </div>
   );
 }
@@ -152,12 +179,6 @@ function MenuAdvanced({ openPanel }: IPanelProps) {
       <MenuItem icon="power" text="Auto shut off" />
       <MenuItem icon="lock" text="Auto standby" />
       <MenuItem icon="time" text="Timer" />
-      <MenuItem
-        icon="media"
-        text="Picture settings"
-        onClick={() => openPanel({ component: MenuPicture, title: "Picture settings" })}
-      />
-      <MenuItem icon="pulse" text="Bitrate" />
   */
   return (
     <div className="Menubar-content">
@@ -172,6 +193,12 @@ function MenuAdvanced({ openPanel }: IPanelProps) {
           onChange={(x) => setFramerate(x)}
         />
       </Label>
+       <MenuItem
+        icon="media"
+        text="Video settings"
+        onClick={() => openPanel({ component: MenuPicture, title: "Video settings" })}
+      />
+      <MenuDivider />
       <Button icon="wrench" text="Factory reset" fill />
       <Button icon="updated" text="Update" fill />
     </div>
@@ -179,16 +206,60 @@ function MenuAdvanced({ openPanel }: IPanelProps) {
 }
 
 function MenuPicture() {
+  const [contrast, setContrast] = useState(0);
+  const [sharpness, setSharpness] = useState(0);
+  const [gain, setGain] = useState(0.0);
+  const [bitrate, setBitrate] = useState(5000000);
+  
   return (
     <div className="Menubar-content">
-      <Menu>
         <MenuItem icon="wrench" text="WB" />
         <MenuItem icon="flash" text="Exposure" />
-        <MenuItem icon="contrast" text="Contrast" />
-        <MenuItem icon="delta" text="Sharpness" />
+        <Label>
+          Contrast
+          <Slider 
+            min={-50}
+            max={100}
+            stepSize={1}
+            labelStepSize={10}
+            value={contrast}
+            onChange={(x) => setContrast(x)}
+          />
+        </Label>
+        <Label>
+          Sharpness
+          <Slider 
+            min={-50}
+            max={100}
+            stepSize={1}
+            labelStepSize={10}
+            value={sharpness}
+            onChange={(x) => setSharpness(x)}
+          />
+        </Label>
         <MenuItem icon="pivot-table" text="Stabilization" />
-        <MenuItem icon="cell-tower" text="Av gain" />
-      </Menu>
+        <Label>
+          Digital gain
+          <Slider 
+            min={-5.0}
+            max={10.0}
+            stepSize={1.0}
+            labelStepSize={5.0}
+            value={gain}
+            onChange={(x) => setGain(x)}
+          />
+        </Label>
+        <Label>
+          Bitrate (x100 000)
+          <Slider 
+            min={5}
+            max={100}
+            stepSize={5}
+            labelStepSize={25}
+            value={bitrate}
+            onChange={(x) => setBitrate(x)}
+          />
+        </Label>
     </div>
   );
 }
